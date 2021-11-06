2a59d322dcb96eecd8f73d06e1e51aab0f6461d1
Gh05t666nero <70060844+Gh05t666nero@users.noreply.github.com>
2021-11-07 00:00:47 +0700

    Update netvus.py

--- netvus.py
+++ netvus.py
@@ -17,13 +17,16 @@ from os import system as cmd
 from bs4 import BeautifulSoup as BS
 import argparse, sys, nmap, socket
 
+
 s = Session()
 p = nmap.PortScanner()
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--target', help='Target IP/Domain')
 parser.add_argument('-s', '--search', help='Search Exploit ID')
 args = parser.parse_args()
+
 
 def banner(target):
     cmd('clear')
@@ -38,44 +41,49 @@ def banner(target):
       ||   /\   )=(   /\\
       []  /  \_/\=/\_/  \\
 -----------|   ABOUT   |-----------{Fore.WHITE}
- SCRIPT    : Network Scanner V0.2
+ SCRIPT    : NETVUS v0.3
  TARGET    : {Fore.YELLOW}{target}{Fore.WHITE}
  AUTHOR    : Gh05t666nero
  WEBSITE   : Deepweb.id
 {Fore.BLUE}-----------|   TOOLS   |-----------''')
 
-def scanning(host):
+
+def netscan(host):
     banner(args.target)
     scan = p.scan(host,'0-1000')
     port = list(scan['scan'][host]['tcp'].keys())
     for tekno in port:
         product = scan['scan'][host]['tcp'][tekno]['product'].strip()
         version = scan['scan'][host]['tcp'][tekno]['version'].strip()
-        outputs = f'{product} {version.strip()}'
-        if version == ' ' or version == '':
-            continue
-        else:
-            headers = {'User-Agent':'Mozilla/5.0 (X11; Linux i686; rv:93.0) Gecko/20100101 Firefox/93.0'}
-            tenable = 'https://www.tenable.com/_next/data/B307Ei_yBxIgZ9c_Tg6vU/en/plugins/search.json?q='+outputs
-            request = s.get(tenable, headers=headers).json()
-            print(f'\n{Fore.BLUE}[!] PORT {Fore.WHITE}{tekno} ({outputs}){Fore.BLUE} TERPAPAR CVE!')
-            plugins = request['pageProps']['plugins']
-            for plugin in plugins:
-                desc = plugin['_source']['description'].split(' ')
-                scid = plugin['_source']['script_id']
-                name = plugin['_source']['script_name']
-                if any(product in word for word in desc) and any(version in word for word in desc):
-                    print(f'''{Fore.RED}             [{scid}] {name}''')
-                    break
+        return [tekno, product, version]
+        
+
+def api_tenable(tekno, product, version):
+    outputs = f'{product} {version.strip()}'
+    if version != ' ' or version != '':
+        headers = {'User-Agent':'Mozilla/5.0 (X11; Linux i686; rv:93.0) Gecko/20100101 Firefox/93.0'}
+        tenable = 'https://www.tenable.com/_next/data/B307Ei_yBxIgZ9c_Tg6vU/en/plugins/search.json?q='+outputs
+        request = s.get(tenable, headers=headers).json()
+        plugins = request['pageProps']['plugins']
+        for plugin in plugins:
+            desc = plugin['_source']['description'].split(' ')
+            scid = plugin['_source']['script_id']
+            name = plugin['_source']['script_name']
+            if any(product in word for word in desc) and any(version in word for word in desc):
+                print(f'\n{Fore.BLUE}[!] PORT {Fore.WHITE}{tekno} ({outputs}){Fore.BLUE} TERPAPAR CVE!')
+                print(f'''{Fore.RED}             [{scid}] {name}''')
+                break
+            else:
+                print(f'\n{Fore.GREEN}[*] PORT {Fore.WHITE}{tekno} ({outputs}){Fore.GREEN} TIDAK TERPAPAR CVE!')        
+                break
+
 
 def searching(exploit_id):
     banner(args.search)
     api_link = 'https://www.tenable.com/plugins/nessus/'+exploit_id
     api_data = s.get(api_link).text
-
     soup = BS(api_data, 'html.parser')
     refe = soup.find_all('section', {'class':'mb-3'})[3].find_all('a')
-
     print(f'''{Fore.WHITE}
 #############################################################
 # TITLE   : {soup.find('meta', {'property':'og:title'})['content'].title()}
@@ -100,7 +108,8 @@ def searching(exploit_id):
 
 
 if args.target is not None and args.search is None:
-    scanning(socket.gethostbyname(args.target))
+    netscan = netscan(socket.gethostbyname(args.target))
+    api_tenable(netscan[0], netscan[1], netscan[2])
 elif args.search is not None and args.target is None:
     searching(args.search)
 else:
